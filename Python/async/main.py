ef3c5a7d1c9fdb3e30f3584c0a38475c69646750
Ezekiel A. Mitchell <ezekielam@icloud.com>
2025-01-20 16:47:37 -0800

    curl + react/fastapi

--- Python/async/main.py
+++ Python/async/main.py
@@ -14,16 +14,43 @@ async def fetch_data(id, delay):
     return {"id ": id, "Data: " : f"Sample data from coroutine id {id}"}
 
 async def main():
+    ### METHOD 1
     # Create tasks for running coroutine concurrently
-    task1 = asyncio.create_task(fetch_data(1, 3))
-    task2 = asyncio.create_task(fetch_data(2, 1))
-    task3 = asyncio.create_task(fetch_data(3, 2))
+    # task1 = asyncio.create_task(fetch_data(1, 3))
+    # task2 = asyncio.create_task(fetch_data(2, 1))
+    # task3 = asyncio.create_task(fetch_data(3, 2))
 
-    result1 = await task1
-    result2 = await task2
-    result3 = await task3
+    # result1 = await task1
+    # result2 = await task2
+    # result3 = await task3
 
-    print(result1, result2, result3)
+    # print(result1, result2, result3)
+
+    ### METHOD 2
+    # results = await asyncio.gather(fetch_data(1, 3), fetch_data(2, 1), fetch_data(3, 2))
+
+    # for result in results:
+    #     print(result)
+
+    ### METHOD 3
+    tasks = []
+
+    async with asyncio.TaskGroup() as tg:
+        for i, sleep_time in enumerate([1,2,3], start=1):
+            task = tg.create_task(fetch_data(i, sleep_time))
+            tasks.append(task)
+
+    # After the Task Group block, all tasks gave completed
+    results = [task.result() for task in tasks]
+
+    for result in results:
+        print(result)
+
+
+
+
+
+
 
 
 # Run the main coroutine.
