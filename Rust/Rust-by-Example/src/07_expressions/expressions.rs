61e275e94638be843a4989eb80b219eb2359ec7c
Ezekiel A. Mitchell <ezekielam@icloud.com>
2025-03-27 13:10:36 -0700

    Day 03

--- Rust/Rust-by-Example/src/07_expressions/expressions.rs
+++ Rust/Rust-by-Example/src/07_expressions/expressions.rs
@@ -1,3 +1,29 @@
+// A Rust program is (mostly) made up of a series of statements
+
+// there are a few kinds of statements in Rust -- the most common two are declaring a variable binding
+// and using a ; with an expression
+
 fn main() {
-    println!("Hello from src/07_expressions/expressions.rs");
+    // variable binding
+    let x = 5;
+
+    // expression
+    x;
+    x + 1;
+    15;
+
+    // blocks are expressions too, so they can be used as values in assignments
+    let y = {
+        let x_squared = x*x;
+        let x_cube = x_squared*x;
+        x_cube + x_squared + x // this expression will be assigned to y
+    };
+
+    let z = {
+        2 + x; // the semicolon ';' supresses the expression and '()' is assigned to 'z'
+    };
+
+    println!("x is {:?}", x);
+    println!("y is {:?}", y);
+    println!("z is {:?}", z);    
 }
