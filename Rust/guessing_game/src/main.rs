c4ea311bea5d3432786b7eec8ae0e4ee68ffe14b
Ezekiel A. Mitchell <ezekielam@icloud.com>
2025-04-21 11:30:05 -0700

    clear repo

--- Rust/guessing_game/src/main.rs
+++ Rust/guessing_game/src/main.rs
@@ -2,32 +2,11 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    println!("//** Guessing Game **//");
+    println!("Guess a number between 1 and 100!");
+    println!("You have 10 attempts to guess the number.");
 
+    let count = 10;
     let secret_number = rand::random_range(1..=100);
 
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
 }
