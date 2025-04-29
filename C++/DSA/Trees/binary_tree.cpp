edd4a60a3bf6ff3b8a4c2a24ccb240a1638e685c
Ezekiel A. Mitchell <ezekielam@icloud.com>
2025-04-28 17:16:40 -0700

    Rust: README.md, C++\DSA

---
+++ C++/DSA/Trees/binary_tree.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left_child;
+    Node* right_child;
+
+    Node(int value) : data(value), left_child(nullptr), right_child(nullptr) {}
+};
+
+fn add(&self) -> Node {
+    return &self;
+};
+
+
+int main() {0;}
