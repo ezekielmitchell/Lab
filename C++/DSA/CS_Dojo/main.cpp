51c41c7a22444b382b2154b1835815ef065f383a
Ezekiel A. Mitchell <ezekielam@icloud.com>
2025-04-29 14:53:07 -0700

    C++ Trees

---
+++ C++/DSA/CS_Dojo/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+// Data structures are organized ways of storing and managing data in a computer's 
+// memory to enable efficient access, insertion, deletion, and manipulation of that data.
+
+using namespace std;
+
+int main() {
+    return 0;
+}
--- C++/DSA/Trees/binary_tree.cpp
+++ C++/DSA/Trees/binary_tree.cpp
@@ -10,9 +10,74 @@ struct Node {
     Node(int value) : data(value), left_child(nullptr), right_child(nullptr) {}
 };
 
-fn add(&self) -> Node {
-    return &self;
-};
+// Search
+bool search(Node* root, int value) {
+    if (root==nullptr) false;
+    if (root->data == value) true;
+    // Recursion method for searching through BST
+    if (value < root->data) {
+        search(root->left_child, value);
+    } else if (value > root->data) {
+        search(root->right_child, value);
+    }
+    return false;
+}
+
+// Insert
+Node* insert(Node* root, int value) {
+    if (root==nullptr) return new Node(value);
+    if (value < root->data) {
+        root->left_child = insert(root->left_child, value);
+    } else if (value > root->data) {
+        root->right_child = insert(root->right_child, value);
+    }
+    return root;
+}
+
+// Delete
+void deleteTree(Node* root) {
+    if (root==nullptr) return;
+    deleteTree(root->left_child);
+    deleteTree(root->right_child);
+    delete root;
+}
+
+// Traversals
+void inOrder(Node* root) {
+    if (root==nullptr) return;
+    inOrder(root->left_child);
+    cout << root->data << " ";
+    inOrder(root->right_child);
+}
+
+void postOrder(Node* root) {
+    if (root==nullptr) return;
+    inOrder(root->left_child);
+    inOrder(root->right_child);
+    cout << root->data << " ";
+}
+
+void preOrder(Node* root) {
+    if (root==nullptr) return;
+    cout << root->data << " ";
+    inOrder(root->left_child);
+    inOrder(root->right_child);
+}
 
 
-int main() {0;}
+int main() {
+    Node* root = new Node(12);
+    insert(root, 2);
+    insert(root, 20);
+    insert(root, 5);
+    insert(root, 32);
+
+    if(search(root, 5)) cout << "Value found!" << endl;
+
+    cout << "Pre-Order Traversal: ";
+    preOrder(root);
+    cout << endl;
+
+    deleteTree(root);
+    return 0;
+}
