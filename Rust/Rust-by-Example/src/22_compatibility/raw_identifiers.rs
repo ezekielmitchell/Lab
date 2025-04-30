0da5a1787224f674afcaba8dc009ec91ff62851f
Ezekiel A. Mitchell <ezekielam@icloud.com>
2025-04-29 22:47:13 -0700

    Rust compatibility

--- Rust/Rust-by-Example/src/22_compatibility/raw_identifiers.rs
+++ Rust/Rust-by-Example/src/22_compatibility/raw_identifiers.rs
@@ -1,3 +1,10 @@
+// Raw identifiers let you use keywords where they would not normally be allowed. 
+// This is particularly useful when Rust introduces new keywords, and a library 
+// using an older edition of Rust has a variable or function with the same name 
+// as a keyword introduced in a newer edition.
+
+// extern crate foo;
+
 fn main() {
-    println!("Hello from src/22_compatibility/raw_identifiers.rs");
+    // foo::r#try();
 }
